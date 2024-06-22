8c08ed103863dad439f693600edb5c7ceea6c3ef
RenatoAguirre <raaguirre1@miuandes.cl>
2024-06-22 15:46:06 -0400

    feat: added read_file funciton

--- src/interpreter.py
+++ src/interpreter.py
@@ -6,10 +6,19 @@ tokens = {
     "➕": "SUM",
     "➖": "SUBTRACTION",
     "‼️": "ASSIGN_ZERO",
-    "➡️": "RIGHT",
-    "⬅️": "LEFT",
+    "➡️": "MOVE_RIGHT",
+    "⬅️": "MOVE_LEFT",
     "🍆": "PRINT_NUMBER",
     "🍑": "PRINT_CHAR",
     "🤣": "COPY",
     "💀": "PASTE",
 }
+
+
+def read_file(file_path: str) -> str:
+    with open(file_path, "r", encoding="utf-8") as file:
+        file_code = file.read().lower()
+    return file_code
+
+
+print(read_file("../ejemplos/hola_mundo.txt"))
