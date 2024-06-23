914e17e73a32180caafa744e2c36c73072fc1437
RenatoAguirre <raaguirre1@miuandes.cl>
2024-06-22 21:15:00 -0400

    feat: now you can interpret code and also added examples

--- src/emoji_script.py
+++ src/emoji_script.py
@@ -1,8 +1,12 @@
+import time
+
+
 class Machine:
-    def __init__(self) -> None:
+    def __init__(self, commands) -> None:
         self.current_position: int = 0
         self.memory: list[int] = [0] * 30000
         self.clipboard: int = 0
+        self.commands = commands  # recibe una lista de tuplas, la tupla [0] es el comando y [1] es el valor
 
     def assing_to_memory(self, value: int):
         self.memory[self.current_position] = value
@@ -23,13 +27,48 @@ class Machine:
         self.current_position -= 1
 
     def show_number_at_position(self):
-        print(self.memory[self.current_position])
+        print(self.memory[self.current_position], end="")
 
     def show_char_at_position(self):
-        print(chr(self.memory[self.current_position]))
+        print(chr(self.memory[self.current_position]), end="")
 
     def copy_to_clipboard(self):
         self.clipboard = self.memory[self.current_position]
 
     def assign_from_clipboard(self):
         self.memory[self.current_position] = self.clipboard
+
+    def run(self):
+
+        # print(self.commands)
+        for command in self.commands:
+            t0 = time.time()
+            # print(command[0])
+            if command[0] == "CODE_START":
+                continue
+            elif command[0] == "ASSIGNMENT":
+                self.assing_to_memory(command[1])
+            elif command[0] == "SUM":
+                self.increment_memory(command[1])
+            elif command[0] == "SUBTRACTION":
+                self.decrease_memory(command[1])
+            elif command[0] == "MOVE_RIGHT":
+                self.move_right()
+            elif command[0] == "MOVE_LEFT":
+                self.move_left()
+            elif command[0] == "PRINT_NUMBER":
+                self.show_number_at_position()
+            elif command[0] == "PRINT_CHAR":
+                self.show_char_at_position()
+            elif command[0] == "COPY":
+                self.copy_to_clipboard()
+            elif command[0] == "ASSIGN_FROM_CLIPBOARD":
+                self.assign_from_clipboard()
+            elif command[0] == "ERASE":
+                self.erease_memory_at_position()
+            elif command[0] == "CODE_END":
+                t1 = time.time()
+                print(f"\nFinsished in {t1-t0} seconds...")
+            else:
+                print("Command not found")
+                break
--- src/interpreter.py
+++ src/interpreter.py
@@ -1,4 +1,7 @@
-tokens = {
+import unicodedata
+from emoji_script import Machine
+
+_tokens = {
     "🔛": "CODE_START",
     "🛑": "CODE_END",
     "💦": "COMMAND_END",
@@ -12,13 +15,113 @@ tokens = {
     "🍑": "PRINT_CHAR",
     "🤣": "COPY",
     "💀": "PASTE",
+    "1️⃣": "1",
+    "2️⃣": "2",
+    "3️⃣": "3",
+    "4️⃣": "4",
+    "5️⃣": "5",
+    "6️⃣": "6",
+    "7️⃣": "7",
+    "8️⃣": "8",
+    "9️⃣": "9",
+    "0️⃣": "0",
 }
+tokens = {
+    "\U0001F51B": "CODE_START",
+    "\U0001F6D1": "CODE_END",
+    "\U0001F4A6": "COMMAND_END",
+    "\U0000270D": "ASSIGNMENT",
+    "\U00002795": "SUM",
+    "\U00002796": "SUBTRACTION",
+    "\U0001F6AB": "ASSIGN_ZERO",
+    "\U000027A1": "MOVE_RIGHT",
+    "\U00002B05 ": "MOVE_LEFT",
+    "\U0001F346": "PRINT_NUMBER",
+    "\U0001F351": "PRINT_CHAR",
+    "\U0001F923": "COPY",
+    "\U0001F480": "PASTE",
+}
+numbers = {
+    "DIGIT ONE": "1",
+    "DIGIT TWO": "2",
+    "DIGIT THREE": "3",
+    "DIGIT FOUR": "4",
+    "DIGIT FIVE": "5",
+    "DIGIT SIX": "6",
+    "DIGIT SEVEN": "7",
+    "DIGIT EIGHT": "8",
+    "DIGIT NINE": "9",
+    "DIGIT ZERO": "0",
+}
+IGNORED_CHARACTERS = [
+    "\n",
+    ",",
+    ";",
+    "?",
+    "¿",
+    "!",
+    "¡",
+    "(",
+    ")",
+    "\t",
+    " ",
+    "\r",
+    "\b",
+]
 
 
-def read_file(file_path: str) -> str:
+def remove_ignored_characters(command_content: str, ignored_characters: list) -> str:
+    """Takes a string and returns the same string, but not including any of the character
+    in IGNORED_CHARACTERS."""
+
+    modified_content = command_content
+
+    for letter in command_content:
+        if letter in ignored_characters:
+            modified_content = modified_content.replace(letter, "")
+
+    return modified_content
+
+
+def numbers_to_value(str: str) -> int:
+    number_list = []
+    for char in str:
+        if unicodedata.name(char) in numbers:
+            number_list.append(numbers[unicodedata.name(char)])
+    # print(int("".join(number_list)))
+    return int("".join(number_list))
+
+
+def read_file(file_path: str) -> list[str]:
     with open(file_path, "r", encoding="utf-8") as file:
-        file_code = file.read().lower()
-    return file_code
+        file_code = file.read().split("💦")
+
+    return [command.strip() for command in file_code]
 
 
-print(read_file("../ejemplos/hola_mundo.txt"))
+def interpret(commands: list[str]):
+    # print("\U0000270D")
+    translated_commands = []
+    for emoji_command in commands:
+        command = tokens[emoji_command[0]]
+        if command == "ASSIGNMENT" or command == "SUM" or command == "SUBTRACTION":
+            number_str = emoji_command[1:]
+            value = numbers_to_value(number_str)
+            translated_commands.append((command, value))
+        else:
+            translated_commands.append((command, 0))
+    ES = Machine(translated_commands)
+
+    ES.run()
+
+
+if __name__ == "__main__":
+    archivo1 = "../ejemplos/hola_mundo.txt"
+    print("corriendo el archivo:", archivo1)
+    commands = read_file(archivo1)
+    interpret(commands)
+
+    archivo2 = "../ejemplos/suma.txt"
+    print("corriendo el archivo:", archivo2)
+    commands = read_file(archivo2)
+    interpret(commands)
